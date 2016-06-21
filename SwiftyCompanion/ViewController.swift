e11b4518cc37982ee89f1827c1221e63a24a49f1
LeTchang <mvallet@student.42.fr>
2016-06-21 14:18:59 +0200

    Error handling enhanced

--- SwiftyCompanion/ViewController.swift
+++ SwiftyCompanion/ViewController.swift
@@ -12,7 +12,6 @@ import SwiftyJSON
 
 class ViewController: UIViewController {
     
-    @IBOutlet var errorView: UIView!
     @IBOutlet weak var loginText: UITextField!
     @IBOutlet weak var loadButton: UIButton!
     
@@ -23,39 +22,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.title = "Mvallet - Intra 42"
         self.view.backgroundColor = UIColor(patternImage: UIImage(imageLiteral: "background"))
-        
-        errorView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.8)
-        errorView.translatesAutoresizingMaskIntoConstraints = false
-        
         auth.get_token()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    func showError() {
-        view.addSubview(errorView)
-        let topConstraint = errorView.topAnchor.constraintEqualToAnchor(view.topAnchor, constant: 150)
-        let leftConstraint = errorView.leftAnchor.constraintEqualToAnchor(view.leftAnchor)
-        let rightConstraint = errorView.rightAnchor.constraintEqualToAnchor(view.rightAnchor)
-        let heightConstraint = errorView.heightAnchor.constraintEqualToConstant(50)
-        NSLayoutConstraint.activateConstraints([topConstraint, leftConstraint, rightConstraint, heightConstraint])
-        view.layoutIfNeeded()
-        self.errorView.alpha = 0
-        UIView.animateWithDuration(0.4) {
-            self.errorView.alpha = 1.0
-        }
-    }
-    
-    func hideError() {
-        UIView.animateWithDuration(0.4, animations: {
-            self.errorView.alpha = 0
-        }) { completed in
-            if completed == true {
-                self.errorView.removeFromSuperview()
-            }
-        }
     }
     
     @IBAction func onLoad(sender: UIButton) {
@@ -65,12 +36,13 @@ class ViewController: UIViewController {
             auth.check_user(login!) {
                 completion in
                 if completion != nil {
-                    self.hideError()
                     self.jsonData = completion
                     self.performSegueWithIdentifier("Profile", sender: nil)
                     self.loadButton.enabled = true
                 } else {
-                    self.showError()
+                    let alert = UIAlertController(title: "Error", message: "This login doesn't exists", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
                     self.loadButton.enabled = true
                 }
             }
